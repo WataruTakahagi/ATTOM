f2df0bd5c7cb190e30cd3b10e54106b5d8c49432
WataruTakahagi <watarut@outlook.com>
2019-02-09 18:56:28 +0900

    Update

--- analyzer.py
+++ analyzer.py
@@ -48,6 +48,9 @@ class pycolor:
 class attom:
     def __init__(self):
         self.filepath = os.getcwd()+'/RunNo.csv'
+        self.analysis_file_name = str(datetime.datetime.today()).split('.')[0].replace('-','').replace(' ','-').replace(':','')+'.anl'
+        f_o = open(self.analysis_file_name,'w')
+        f_o.close()
 
     def run_no(self):
         if os.path.exists(self.filepath) == True:
@@ -73,7 +76,7 @@ class attom:
             run_no = open('RunNo.csv','r')
 
     def binary2txt(self):
-        jobid = 1
+        f_o,jobid = open(self.analysis_file_name,'a'),1
         data_list = [i for i in glob.glob('*') if not '.py' in i and not '.txt' in i and not '.csv' in i and not '.xls' in i and not '.anl' in i]
         data_list = sorted(data_list)
         if 'data' in data_list:del data_list[data_list.index('data')]
@@ -84,6 +87,7 @@ class attom:
             fo = open(fname+'.txt','w')
             for line in fi:fo.write(line)
             print(pycolor.RED+'JOB_id = data '+str(jobid)+' '+pycolor.END+fname+'* > '+fname+'.txt > '+pycolor.BLUE+'data'+pycolor.END)
+            f_o.write('JOB_id = data '+str(jobid)+' '+fname+'* > '+fname+'.txt > data'+'\n')
             jobid += 1
             fo.close()
         data_list = [i for i in glob.glob('*.txt')]
@@ -91,10 +95,11 @@ class attom:
         filepath = os.getcwd()+'/data'
         if os.path.exists(filepath) == False:os.system('mkdir data')
         os.system('mv *.txt data')
+        f_o.close()
         print(pycolor.REVERCE+pycolor.BLUE+'data'+pycolor.REVERCE+pycolor.END)
 
     def txt2xls(self):
-        jobid = 1
+        f_o,jobid = open(self.analysis_file_name,'a'),1
         wb = xlwt.Workbook()
         filepath = os.getcwd()+'/data/*.txt'
         for sheetname in glob.glob(filepath):
@@ -106,12 +111,14 @@ class attom:
                     sheet.write(line, j, i[j])
                 line += 1
             print(pycolor.RED+'JOB_id = integrate '+str(jobid)+' '+pycolor.END+sheetname.split('/')[-1])
+            f_o.write('JOB_id = integrate '+str(jobid)+' '+sheetname.split('/')[-1]+'\n')
             jobid += 1
         wb.save('attom_data.xls')
+        f_o.close()
         print(pycolor.REVERCE+pycolor.BLUE+'attom_data.xls'+pycolor.REVERCE+pycolor.END)
 
     def txt2png(self):
-        jobid = 1
+        f_o,jobid = open(self.analysis_file_name,'a'),1
         filepath = os.getcwd()+'/data/*.txt'
         data_list = [i for i in glob.glob(filepath)]
         data_list = sorted(data_list)
@@ -127,10 +134,12 @@ class attom:
                 y = [float(i) for i in array.T[j+2][1:]]
                 self.genpng(x,y,name)
                 print(pycolor.RED+'JOB_id = plot '+str(jobid)+' '+pycolor.END+name+'.png > '+pycolor.BLUE+'figure'+pycolor.END)
+                f_o.write('JOB_id = plot '+str(jobid)+' '+name+'.png > figure'+'\n')
                 jobid += 1
         filepath = os.getcwd()+'/figure'
         if os.path.exists(filepath) == False:os.system('mkdir figure')
         os.system('mv *.png figure')
+        f_o.close()
         print(pycolor.REVERCE+pycolor.BLUE+'figure'+pycolor.REVERCE+pycolor.END)
 
     def genpng(self,x,y,name):
@@ -152,11 +161,10 @@ class attom:
         plt.close()
 
     def analysis(self):
+        f_o,jobid = open(self.analysis_file_name,'a'),1
         f,sample_id,blank = open(self.filepath).readlines(),[],str()
         for i in f:print('Run No. =',pycolor.GREEN+i.rstrip().split(',')[0]+pycolor.END,'/ Sample ID =',pycolor.GREEN+i.rstrip().split(',')[1]+pycolor.END,'/ Sample name =',pycolor.GREEN+i.rstrip().split(',')[2]+pycolor.END)
         judge = input('Correct '+pycolor.GREEN+'Run No. '+pycolor.END+'&'+pycolor.GREEN+' Sample Name '+pycolor.END+'? '+pycolor.BLUE+'(y/n) '+pycolor.END)
-        analysis_file_name = str(datetime.datetime.today()).split('.')[0].replace('-','').replace(' ','-').replace(':','')+'.anl'
-        f_o = open(analysis_file_name,'w')
         if judge == 'y':
             #mincyc, maxcyc = int(input('Cycle minimum = ')), int(input('Cycle maximum = '))
             lane, mincyc, maxcyc, blank_dic, sample_dic, blankcount = 0, 40, 100, {},{}, 0
@@ -166,11 +174,12 @@ class attom:
                         print('--- Calculating '+pycolor.BLUE+'mean(sample)'+pycolor.END+' - '+pycolor.BLUE+'mean(blank)'+pycolor.END+' ---')
                         for db in list(blank_dic.keys()):
                             text = ','.join([str(logname),str(db),str((sample_dic[db] - blank_dic[db]))])
-                            f_o.write(text+'\n')
-                            print('Sample name =',pycolor.GREEN+str(logname)+pycolor.END,'/ Element =',pycolor.GREEN+str(db)+pycolor.END,'/ Value =',pycolor.GREEN+str((sample_dic[db] - blank_dic[db]))+pycolor.END)
+                            print(pycolor.RED+'JOB_id = analysis '+str(jobid)+' '+pycolor.END+'Sample name =',pycolor.GREEN+str(logname)+pycolor.END,'/ Element =',pycolor.GREEN+str(db)+pycolor.END,'/ Value =',pycolor.GREEN+str((sample_dic[db] - blank_dic[db]))+pycolor.END)
+                            f_o.write('JOB_id = analysis '+str(jobid)+' '+text+'\n')
+                            jobid += 1
                         print('--- Calculated ! -----------------------------')
                     if lane == 0: blank_dic, sample_dic = {},{}
-                    print('Queue =',pycolor.BLUE+i.rstrip().split(',')[0]+pycolor.END,'/ Sample ID =',pycolor.BLUE+i.rstrip().split(',')[1]+pycolor.END,'/ Sample name =',pycolor.BLUE+i.rstrip().split(',')[2]+pycolor.END)
+                    print(pycolor.RED+'Queue'+pycolor.END+' =',pycolor.BLUE+i.rstrip().split(',')[0]+pycolor.END,'/ Sample ID =',pycolor.BLUE+i.rstrip().split(',')[1]+pycolor.END,'/ Sample name =',pycolor.BLUE+i.rstrip().split(',')[2]+pycolor.END)
                     lane = 0
                     f_anl, frag = open('data/'+i.rstrip().split(',')[0]+'.txt','r').readlines(),0
                     for line in f_anl:
@@ -185,7 +194,7 @@ class attom:
                             blank_dic[name] = np.mean([blank_dic[name],np.mean(y[mincyc-1:maxcyc])])
                     lane += 1; blankcount += 1
                 if i.rstrip().split(',')[1] == 'sample':
-                    print('Queue =',pycolor.BLUE+i.rstrip().split(',')[0]+pycolor.END,'/ Sample ID =',pycolor.BLUE+i.rstrip().split(',')[1]+pycolor.END,'/ Sample name =',pycolor.BLUE+i.rstrip().split(',')[2]+pycolor.END)
+                    print(pycolor.RED+'Queue'+pycolor.END+' =',pycolor.BLUE+i.rstrip().split(',')[0]+pycolor.END,'/ Sample ID =',pycolor.BLUE+i.rstrip().split(',')[1]+pycolor.END,'/ Sample name =',pycolor.BLUE+i.rstrip().split(',')[2]+pycolor.END)
                     logname = i.rstrip().split(',')[2]
                     f_anl, frag, blankcount = open('data/'+i.rstrip().split(',')[0]+'.txt','r').readlines(),0,0
                     for line in f_anl:
@@ -208,7 +217,7 @@ class attom:
         filepath = os.getcwd()+'/archive'
         if os.path.exists(filepath) == False:os.system('mkdir archive')
         os.system('mv *.anl archive')
-        print(pycolor.REVERCE+pycolor.BLUE+analysis_file_name+pycolor.REVERCE+pycolor.END)
+        print(pycolor.REVERCE+pycolor.BLUE+self.analysis_file_name+pycolor.REVERCE+pycolor.END)
 
     def target_Search(self, target):
         filepath = os.getcwd()+'/'+target
